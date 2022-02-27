57519fb480c31d454b8c87e1fe3884a9331e64ad
Neal Wicke <neal.wicke@hotmail.com>
2022-02-27 13:47:19 +0100

    End sect 16

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,121 @@
+import useInput from "../hooks/use-input";
+import validator from "validator";
+
 const BasicForm = (props) => {
+    const {
+      value: enteredName,
+      isValid: enteredNameIsValid,
+      hasError: nameInputHasError,
+      valueChangeHandler: nameChangedHandler,
+      inputBlurHandler: nameBlurHandler,
+      reset: resetNameInput,
+    } = useInput((value) => value.trim() !== "");
+
+    const {
+      value: enteredLastName,
+      isValid: enteredLastNameIsValid,
+      hasError: lastNameInputHasError,
+      valueChangeHandler: lastNameChangedHandler,
+      inputBlurHandler: lastNameBlurHandler,
+      reset: resetLastNameInput,
+    } = useInput((value) => value.trim() !== "");
+
+    const {
+      value: enteredEmail,
+      isValid: enteredEmailIsValid,
+      hasError: emailInputHasError,
+      valueChangeHandler: emailChangedHandler,
+      inputBlurHandler: emailBlurHandler,
+      reset: resetEmailInput,
+    } = useInput((value) => validator.isEmail(value));
+
+  
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid && enteredLastNameIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (
+      !enteredNameIsValid &&
+      !enteredEmailIsValid &&
+      !enteredLastNameIsValid
+    ) {
+      return;
+    }
+
+    console.log(enteredName);
+    console.log(enteredEmail);
+    console.log(enteredLastName);
+    resetNameInput();
+    resetEmailInput();
+    resetLastNameInput();
+  };
+
+  const nameInputClasses = nameInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  const lastNameInputClasses = lastNameInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = emailInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <div className={nameInputClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={nameChangedHandler}
+            onBlur={nameBlurHandler}
+            value={enteredName}
+          />
+          {nameInputHasError && (
+            <p className="error-text">Name must not be empty.</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastNameInputClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={lastNameChangedHandler}
+            onBlur={lastNameBlurHandler}
+            value={enteredLastName}
+          />
+          {lastNameInputHasError && (
+            <p className="error-text">Last name must not be empty.</p>
+          )}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailInputClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          onChange={emailChangedHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && (
+          <p className="error-text">Email must not be empty.</p>
+        )}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+      <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
 };
+
 
 export default BasicForm;
